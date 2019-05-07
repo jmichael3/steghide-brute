10855cdfc4d7c668a73dff03a1505a02ae6f691a
root <root@localhost.localdomain>
2019-05-07 00:42:57 -0400

    pep8 format

--- steghide-brute.py
+++ steghide-brute.py
@@ -3,17 +3,19 @@
 import sys
 import subprocess
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # do magic
     if len(sys.argv) != 3:
         print("\n[*]Usage: {} <image_file> <wordlist>\n".format(sys.argv[0]))
         sys.exit()
     else:
-        with open(sys.argv[2],"r") as f:
+        with open(sys.argv[2], "r") as f:
             for _ in f.readlines():
                 print("[*]Trying... {}".format(_))
                 try:
-                    subprocess.check_output("steghide extract -sf {} -p {}".format(sys.argv[1],_),shell=True) # steghide extract -sf image.jpg -p password123
+                    # steghide extract -sf image.jpg -p password123
+                    subprocess.check_output(
+                        "steghide extract -sf {} -p {}".format(sys.argv[1], _), shell=True)
                     print("[+]Success : {}".format(_))
                     sys.exit()
                 except Exception as e:
